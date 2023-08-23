494f491e4aa39fbf14076bbbc4ef5e845ea2a07e
DiogoQuintinoSilva <142818291+DiogoQuintinoSilva@users.noreply.github.com>
2023-08-23 10:03:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,21 @@
 import streamlit
 import pandas
+import requests
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
 
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+# fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(fruits_to_show.index))
+# streamlit.multiselect("Pick some fruits:", list(fruits_to_show.index))
 
 # Display the table on the page.
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
